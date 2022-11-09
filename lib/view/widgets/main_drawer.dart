6424233b0acf6cfe14eacbd78990885d38c3427e
Wasim Zaman <wasimxaman13@gmail.com>
2022-11-09 20:45:01 +0500

    Drawer links added

--- lib/view/widgets/main_drawer.dart
+++ lib/view/widgets/main_drawer.dart
@@ -3,23 +3,66 @@ import 'package:flutter/material.dart';
 class MainDrawer extends StatelessWidget {
   const MainDrawer({super.key});
 
+  void switchPage(BuildContext context) {}
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: Theme.of(context).accentColor,
       child: Column(
         children: [
           Container(
             height: MediaQuery.of(context).size.height * 0.2,
             decoration: BoxDecoration(
-              color: Theme.of(context).accentColor,
-              border: Border.all(width: 4),
+              color: Theme.of(context).primaryColor,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(13),
+                bottomRight: Radius.circular(13),
+              ),
             ),
             child: Center(
-                child: Text(
-              'Warm Meal!',
-              style: Theme.of(context).textTheme.headline3,
-            )),
-          )
+              child: Text(
+                'Warm Meal!',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.food_bank_outlined,
+              size: 50,
+            ),
+            title: const Text(
+              'Meals',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            onTap: () {
+              switchPage(context);
+            },
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.filter,
+              size: 50,
+            ),
+            title: const Text(
+              'Filters',
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+            onTap: () {
+              switchPage(context);
+            },
+          ),
         ],
       ),
     );
